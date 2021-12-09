27db3de8dcca5dc85a54e75dc72645095ac2b132
Chen Junda <ddadaal@outlook.com>
2021-12-09 15:18:07 +0800

    feat(utils): add portOrZero envalid validator

--- packages/utils/src/config.ts
+++ packages/utils/src/config.ts
@@ -1,4 +1,4 @@
-import { customCleanEnv, strictProxyMiddleware, ValidatorSpec } from "envalid";
+import { customCleanEnv, EnvError, makeValidator, Spec, strictProxyMiddleware, ValidatorSpec } from "envalid";
 
 // convert ${a} to process.env.a
 function parsePlaceholder<T extends Record<string, any>>(env: T, rawEnv: any) {
@@ -19,4 +19,23 @@ export const envConfig = <T extends object>(
       strictProxyMiddleware(parsePlaceholder(env, rawEnv), rawEnv),
   );
 
+// eslint-disable-next-line max-len
+// https://github.com/af/envalid/blob/086609b33f7f725042ecbfbb381a8fd61cca993b/src/validators.ts?_pjax=%23js-repo-pjax-container%2C%20div%5Bitemtype%3D%22http%3A%2F%2Fschema.org%2FSoftwareSourceCode%22%5D%20main%2C%20%5Bdata-pjax-container%5D#L89
+export function portOrZero<T extends number = number>(spec?: Spec<T>) {
+  return makeValidator((input: string) => {
+    const coerced = +input;
+    if (
+      Number.isNaN(coerced) ||
+        `${coerced}` !== `${input}` ||
+        coerced % 1 !== 0 ||
+        coerced < 0 ||
+        coerced > 65535
+    ) {
+      throw new EnvError(`Invalid port input: "${input}"`);
+    }
+    return coerced as T;
+  })(spec);
+}
+
 export * as envalid from "envalid";
+

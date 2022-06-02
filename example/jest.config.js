c496c782d3075c4d78da37c07fe09a3f52d3c135
Chen Junda <ddadaal@outlook.com>
2022-06-02 06:56:23 +0000

    chore: ensure tests pass

--- example/jest.config.js
+++ example/jest.config.js
@@ -1,5 +1,5 @@
 // jest.config.js
-const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { pathsToModuleNameMapper } = require("ts-jest");
 // In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
 // which contains the path mapping (ie the `compilerOptions.paths` option):
 const { compilerOptions } = require("./tsconfig");
